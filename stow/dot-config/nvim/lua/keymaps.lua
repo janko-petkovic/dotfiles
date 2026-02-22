b9adc205e07467428b081eb2b90b0f8c2ebb3ce8
Janko Petkovic <j.petkovic91@gmail.com>
2026-02-23 00:56:37 +0100

    [NVIM] Updated keymaps, installed markdown-preview-nvim

--- stow/dot-config/nvim/lua/keymaps.lua
+++ stow/dot-config/nvim/lua/keymaps.lua
@@ -10,9 +10,28 @@ local lang_exec_table = {
 map('i', 'jk', '<Esc>')
 map('n', 'J', 'J^')
 
+-- Windows
+--Moving
+map('n', '<C-l>', '<C-w>l')
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-j>', '<C-w>j')
+--Resizing
+map('n', '=', '<cmd>horizontal resize +1<cr>')
+map('n', '+', '<cmd>horizontal resize -1<cr>')
+map('n', '-', '<cmd>vertical resize +1<cr>')
+map('n', '_', '<cmd>vertical resize -1<cr>')
+--Splitting
+map('n', '<leader>s', '<cmd>sp<cr>')
+map('n', '<leader>v', '<cmd>vs<cr>')
+
+-- Dianostics
 map('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Floating diagnostics'})
 map('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list'})
 
+
+-- Utils
+--Running (broken)
 map('n', '<leader>r', function()
     local file_name = vim.fn.expand("%")
     local file_type = vim.bo.filetype
@@ -20,3 +39,8 @@ map('n', '<leader>r', function()
   end,
   { desc = 'Run the currently open buffer' }
 )
+
+
+--Sourcing
+map('n', '<leader>w', function()
+  vim.api.nvim_cmd({cmd="source", args={vim.fn.expand("%")}}, {}) end, {})
