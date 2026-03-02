71cd55c871554c10d006902428b1869a27cca961
Janko Petkovic <j.petkovic91@gmail.com>
2026-03-02 11:53:46 +0100

    Branched stow folder for lazarus

---
+++ stow_lazarus/dot-config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+  'nvim-telescope/telescope.nvim', version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- optional but recommended
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+
+  config = function()
+    local map = vim.keymap.set
+    local builtin = require('telescope.builtin')
+
+    map('', '<leader>pl', builtin.planets, {})
+    map('', '<leader><leader>', builtin.builtin, {})
+    map('', '<leader>ff', builtin.find_files, {})
+    map('', '<leader>?', builtin.oldfiles, {})
+    map('', '<leader>/', builtin.buffers, {})
+    map('', '<leader>gs', builtin.live_grep, {})
+    map('', '<leader>gs', builtin.live_grep, {})
+
+    require('telescope').load_extension('refactoring')
+    map({'n','x'}, '<leader>rf', function() 
+        require('telescope').extensions.refactoring.refactors()
+      end,
+      {}
+    )
+  end
+}
+
+
